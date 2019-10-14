b2b5df2e91bee02fce832e713820a2b916d5d510
Jannis Vamvas <vamvas@cl.uzh.ch>
2019-10-14 13:57:14 +0200

    Add code

---
+++ script.js
@@ -0,0 +1,27 @@
+$(function () {
+
+    $("#word").keyup(function () {
+        const inputWord = $(this).val();
+
+        if (!inputWord.trim()) {
+            // Input is empty
+            $(".palindrome-message").hide();
+            return
+        }
+
+        $(".query").text(inputWord);
+
+        if (isPalindrome(inputWord)) {
+            // TODO
+        } else {
+            // TODO
+        }
+
+        $(".palindrome-message").show();
+    });
+
+});
+
+function isPalindrome(word) {
+    return word.toLowerCase() === word.split('').reverse().join('');
+}
